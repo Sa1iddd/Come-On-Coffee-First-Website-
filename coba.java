69154ec28f0315082666dff64a870092b2178f44
Saiiddd <muhzackysaid@gmail.com>
2024-10-03 13:56:28 +0700

    website kopi (first website)

---
+++ coba.java
@@ -0,0 +1,6 @@
+public class coba{
+    public static void main (String[] args) {
+        String a = new String("World");
+        System.out.printf("Hello " + a);
+    }
+}
---
+++ wk.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class wk {
+    public static void main(String[] args) throws Exception {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter name: ");
+        String userNama = myObj.nextLine();
+        System.out.println("Username is: " + userNama);
+    }
+}
